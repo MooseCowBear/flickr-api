adfef1c068c5d5f69b9c27d875af5c1e112d9057
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-05-30 15:57:41 -0400

    edit comment

--- app/models/concerns/photo.rb
+++ app/models/concerns/photo.rb
@@ -3,7 +3,7 @@ class Photo
     unless search_params.empty?
       f = FlickrService.new
       @photo_data = f.get_photos_by_id(search_params[:user_id])
-      [@photo_data, true] #this may contain photos but also might contain just failure message
+      [@photo_data, true] #this may contain photos, or not. but still want the error.
     else
       @photo_data = {}
       [@photo_data, false]
